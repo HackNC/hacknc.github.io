0731c28915df406c4b971a1ad5c479549921bf59
Yicheng Wang <alex.wyc2098@protonmail.ch>
2018-09-30 22:17:29 -0400

    moved to be up-to-date

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,46 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $("#space-holder").height($('nav').height());
     $('.modal').modal();
+
+    $.getJSON("/static/assets/schedule.json", function(data) {
+        data.forEach(function(date) {
+          var i = 0;
+            $("#schedule-container").append(
+               $('<h3>')
+                 .text(date['date'])
+            );
+
+            var $table = $('<table>', {'class': 'schedule'});
+            var $schBody = $('<tbody>');
+
+            $table.append(
+                $('<thead>').append(
+                    $('<tr>')
+                      .append($('<th>', {"style": "width:25%;"}).text('Time'))
+                      .append($('<th>', {"style": "width:55%;"}).text('Event'))
+                      .append($('<th>', {'style': "width:20%;"}).text('Location'))
+                )
+            );
+
+            date['schedule'].forEach(function(element) {
+                var row = $('<tr>')
+                row.append('<td>')
+                $schBody.append(
+                    $('<tr>', {"class" : (i++ % 2 == 0 ? "table-row-even" : "table-row-odd")})
+                      .append($('<td>').text(element['time']))
+                      .append($('<td>').text(element['event']))
+                      .append($('<td>').text(element['location']))
+                )
+            });
+
+            console.log($schBody);
+
+            var $alltable = $('<table>', {'class': 'schedule'});
+
+            $table.append($schBody);
+            $('#schedule-container').append($table);
+        });
+    });
 });
 
 // Smooth scroll to hash -- selects each used link with hashes.
